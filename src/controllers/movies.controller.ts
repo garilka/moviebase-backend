147cd09a1acce52604a1dfea611855896d46133f
Ilka Garai <ilka.garai@tappointment.com>
2024-05-25 16:59:26 +0200

    MB-dev feat: movies controller

---
+++ src/controllers/movies.controller.ts
@@ -0,0 +1,32 @@
+import { CustomError } from '../errors/customErrorClass.ts';
+import { moviesService } from '../services/movies.service.ts';
+import { queriesService } from '../services/queries.service.ts';
+
+type GetExternalMoviesProps = {
+  search: string;
+  page?: number;
+};
+
+const getExternalMovies = async ({ search, page }: GetExternalMoviesProps) => {
+  const query = `search=${search}&page=${page}`;
+
+  try {
+    const externalApiResponse = await moviesService.fetchExternalMovies({ search, page });
+
+    const createdQuery = await queriesService.createQuery({ search, externalApiResponse });
+
+    const createdOrUpdatedMovieIds = await moviesService.putMovies(externalApiResponse, query);
+
+    await moviesService.connectMoviesToQuery(createdOrUpdatedMovieIds, createdQuery.id);
+
+    const moviesForCachedQuery = await moviesService.findMoviesByQuery(query, true);
+
+    return moviesForCachedQuery;
+  } catch (error) {
+    throw new CustomError('Error occured during get movies from external API', error);
+  }
+};
+
+export const moviesController = {
+  getExternalMovies,
+};
