6ceb9c4ab0286101ad3c51882bae71147c13fc84
Ilka Garai <ilka.garai@tappointment.com>
2024-05-26 14:12:01 +0200

    MB-dev fix: invalid release date from 3rd parti API

--- src/utils/generateMoviesData.ts
+++ src/utils/generateMoviesData.ts
@@ -1,19 +1,18 @@
 import { Prisma } from '@prisma/client';
 import { ExternalMoviesResponse } from '../types/movies.types.ts';
+import { convertToValidStringDate } from './convertToValidDate.ts';
 import { generatePosterUrl } from './generatePosterUrl.ts';
 
 export const generateMoviesData = (externalMoviesResponse: ExternalMoviesResponse): Prisma.MovieCreateInput[] => {
   const internalMovies: Prisma.MovieCreateInput[] = externalMoviesResponse?.results?.map((externalMovie) => {
     const { id, title, genre_ids, release_date, overview, vote_average, original_title, poster_path } = externalMovie;
-    const date = new Date().toISOString();
-    // NEED FIX
-    // sometimes the 3rd parti API return release_date that has invalid form, can' be converted to ISO string
+    const releaseDate = convertToValidStringDate(release_date);
 
     return {
       id: +id,
       title,
       genreIds: genre_ids,
-      releaseDate: date,
+      releaseDate,
       rawData: JSON.stringify(externalMovie),
       ...(overview && { overview }),
       ...(vote_average && { voteAverage: vote_average }),
