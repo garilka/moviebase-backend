17b73fd8082449d10bf74138b8580f5f3980f14f
Ilka Garai <ilka.garai@tappointment.com>
2024-05-27 22:04:49 +0200

    MB-dev fix: poster base url in config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,6 @@ export const config = {
   redisBaseUrl: process.env.REDIS_BASE_URL ?? 'redis://127.0.0.1:',
   redisPort: process.env.REDIS_PORT ? +process.env.REDIS_PORT : 6379,
   apiMovieBaseUrl: process.env.API_MOVIE_BASE_URL ?? 'https://api.themoviedb.org/3/search/movie',
-  apiPosterBaseUrl: process.env.API_MOVIE_BASE_URL ?? 'https://image.tmdb.org/t/p/w500e',
+  apiPosterBaseUrl: process.env.API_POSTER_BASE_URL ?? 'https://image.tmdb.org/t/p/w500',
   cacheMinute: process.env.CACHE_TIME_MINUTE ? +process.env.CACHE_TIME_MINUTE : 2,
 };
